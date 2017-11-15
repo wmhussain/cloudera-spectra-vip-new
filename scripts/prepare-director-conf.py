2575503408b95b35765fc3091d79e15281f8f283
Mazhar Hussain Warsi <warsi.mazhar@gmail.com>
2017-11-15 14:38:04 +0530

    Update prepare-director-conf.py

--- scripts/prepare-director-conf.py
+++ scripts/prepare-director-conf.py
@@ -177,6 +177,10 @@ def prepareAndImportConf(options):
                           networkSecurityGroup,
                           virtualNetworkResourceGroup, virtualNetwork, subnetName,
                           computeResourceGroup, hostFqdnSuffix)
+    setInstanceParameters(conf, 'instances.gateway', gatewayType, networkSecurityGroupResourceGroup,
+                          networkSecurityGroup,
+                          virtualNetworkResourceGroup, virtualNetwork, subnetName,
+                          computeResourceGroup, hostFqdnSuffix)
     setInstanceParameters(conf, 'cloudera-manager.instance', edgeType,
                           networkSecurityGroupResourceGroup, networkSecurityGroup,
                           virtualNetworkResourceGroup, virtualNetwork, subnetName,
@@ -186,6 +190,10 @@ def prepareAndImportConf(options):
                           virtualNetworkResourceGroup, virtualNetwork, subnetName,
                           computeResourceGroup, hostFqdnSuffix)
     setInstanceParameters(conf, 'cluster.workers.instance', masterType,
+                          networkSecurityGroupResourceGroup, networkSecurityGroup,
+                          virtualNetworkResourceGroup, virtualNetwork, subnetName,
+                          computeResourceGroup, hostFqdnSuffix)
+    setInstanceParameters(conf, 'cluster.gateway.instance', gatewayType,
                           networkSecurityGroupResourceGroup, networkSecurityGroup,
                           virtualNetworkResourceGroup, virtualNetwork, subnetName,
                           computeResourceGroup, hostFqdnSuffix)
